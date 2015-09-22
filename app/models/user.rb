7e8f4e7f0d2e1d50f28049e2c583b13e9523c7e1
kangkyu <kangkyu1111@gmail.com>
2015-09-21 23:29:08 -0700

    generate scaffold of user model

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+
+  def self.find_or_create_from_auth_hash(auth_hash)
+    token = auth_hash.credentials.token
+    email = auth_hash.info.email
+
+    User.find_or_create_by(email:email) do |user|
+      user.token = token
+    end
+
+  end
+end
