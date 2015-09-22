42c50b0a50a24cb4ba76989ceb036d63cba5c2b2
kangkyu <kangkyu1111@gmail.com>
2015-09-22 08:40:28 -0700

    work in process

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :current_user
+  def current_user
+    @current_user
+  end
+
+  def index
+    binding.pry
+    @user = current_user
+  end
+
   def current_user=(user)
     @current_user = user
   end
