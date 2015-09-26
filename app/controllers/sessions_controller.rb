4354460de7191b9134a0a54c5478143fba3018a8
kangkyu <kangkyu1111@gmail.com>
2015-09-25 19:45:24 -0700

    ignore local docs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,8 @@
 class SessionsController < ApplicationController
   def create
+    # raise auth_hash.to_yaml
     @user = User.find_or_create_from_auth_hash(auth_hash)
-
     session[:current_user_id] = @user.id
-
     self.current_user = @user
     redirect_to '/'
   end
